61ff68811c7ddd9efbaa7ec508e99cac0161ed3f
Atlon1 <maciej.szajstek@gmail.com>
2022-08-16 20:09:54 +0200

    Routing aktywowany

--- rent-stuff/src/App.js
+++ rent-stuff/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import '../../scss/main.scss';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
 
 
 
 export default function App () {
   return (
-      <>
-      </>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
+      </BrowserRouter>
   )
 }
---
+++ rent-stuff/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../scss/main.scss';
+
+const Home = () => {
+
+    return (
+        <section className='home' id='home'>
+            <div className='wrapper'>
+                <div className='home__container'>
+                    ssgsgsg
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
