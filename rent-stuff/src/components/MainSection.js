6171fc2849bcea257663184c7f02fb8a71febc8f
Atlon1 <maciej.szajstek@gmail.com>
2022-08-18 22:23:39 +0200

    Sekcja Home gotowa

--- rent-stuff/src/components/MainSection.js
+++ rent-stuff/src/components/MainSection.js
@@ -1,15 +1,22 @@
 import React from "react";
 import '../scss/main.scss';
+import {Link} from "react-router-dom";
+import decoration from "../assets/Decoration.svg";
+
 
 const MainSection = () => {
 
     return (
         <section className='mainSection'>
             <div className='wrapper'>
+                <div className='mainSection_img'/>
                 <div className='mainSection__container'>
-                    <h1> Zacznij pomagac!</h1>
-                    <p> Oddaj niechciane rzeczy w zaufane ręce</p>
-                    <img src={require('../assets/Decoration.svg')}/>
+                    <h1> Zacznij pomagać! <br/>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <img className='decoration' src={decoration}/>
+                    <div className='button__container'>
+                        <button className="big-btn"><Link to="Login">Oddaj<br/>rzeczy</Link></button>
+                        <button className="big-btn"><Link to="Register">Zorganizuj<br/>zbiórkę</Link></button>
+                    </div>
                 </div>
             </div>
         </section>
