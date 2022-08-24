7a027d72be31c9460b843d378ee6d94d2d56a5be
Atlon1 <maciej.szajstek@gmail.com>
2022-08-24 21:31:25 +0200

    sekcja Simple cala gotowa

--- rent-stuff/src/components/SimpleSteps.js
+++ rent-stuff/src/components/SimpleSteps.js
@@ -18,26 +18,30 @@ const SimpleSteps = () => {
                     <div className='simpleSteps__content'>
                         <div className='simpleSteps__firstContent'>
                             <img className='simpleSteps__icon1' src={icon1}/>
-                            <div className='simpleSteps__header'>Wybierz Rzeczy</div>
+                            <div className='simpleSteps__header'>Wybierz rzeczy</div>
+                            <span> </span>
                             <p className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
                         </div>
                         <div className='simpleSteps__secContent'>
                             <img className='simpleSteps__icon2' src={icon2}/>
-                            <div className='simpleSteps__header'>Wybierz Rzeczy</div>
-                            <p className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
+                            <div className='simpleSteps__header'>Spakuj je</div>
+                            <span> </span>
+                            <p className='simpleSteps__text'>skorzystaj z worków na śmieci</p>
                         </div>
                         <div className='simpleSteps__thirdContent'>
                             <img className='simpleSteps__icon3' src={icon3}/>
-                            <div className='simpleSteps__header'>Wybierz Rzeczy</div>
-                            <p className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
+                            <div className='simpleSteps__header'>Zdecyduj komu chcesz pomóc</div>
+                            <span> </span>
+                            <p className='simpleSteps__text'>wybierz zaufane miejsce</p>
                         </div>
                         <div className='simpleSteps__fourContent'>
                             <img className='simpleSteps__icon4' src={icon4}/>
-                            <div className='simpleSteps__header'>Wybierz Rzeczy</div>
-                            <p className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
+                            <div className='simpleSteps__header'>Zamów kuriera</div>
+                            <span> </span>
+                            <p className='simpleSteps__text'>kurier przyjedzie w dogodnym terminie</p>
                         </div>
-                        <button className="big-btn"><Link to="Login">ODDAJ<br/>RZECZY</Link></button>
                     </div>
+                    <button className="big-btn"><Link to="Login">ODDAJ<br/>RZECZY</Link></button>
                 </div>
             </div>
         </section>
