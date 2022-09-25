c7dc82e158dd1cfaa44d99c15ada9950efc479e2
Atlon1 <maciej.szajstek@gmail.com>
2022-09-25 18:02:15 +0200

    Form_section_CSS_and_Component

--- rent-stuff/src/components/logeedComponents/FormSection.js
+++ rent-stuff/src/components/logeedComponents/FormSection.js
@@ -4,9 +4,15 @@ import '../../scss/main.scss';
 const FormSection = () => {
 
     return(
-        <section className='from'>
+        <section className='form'>
             <div className="wrapper">
-                <div>Przykładowy formularz</div>
+                <div className='form__yellowBar'>
+                    <div className='form__yellowBar__header'>Ważne!</div>
+                    <div className='form__yellowBar__content'>Uzupłenij szczegóły dotczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</div>
+                </div>
+                <div className='form__pageOne'>
+                    <div className='form__pageOne__steps'> Krok 1/4</div>
+                </div>
             </div>
         </section>
     )
