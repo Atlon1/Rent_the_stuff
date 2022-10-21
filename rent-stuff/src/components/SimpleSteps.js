876b0aca1d175d96ae00078dc8b5c47b2d52cd8f
Atlon1 <maciej.szajstek@gmail.com>
2022-10-21 18:49:55 +0200

    section simpleSteps RWD redy

--- rent-stuff/src/components/SimpleSteps.js
+++ rent-stuff/src/components/SimpleSteps.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../scss/main.scss";
 import decoration from '../assets/Decoration.svg'
 import icon1 from '../assets/Icon-1.svg'
@@ -6,8 +6,14 @@ import icon2 from '../assets/Icon-2.svg'
 import icon3 from '../assets/Icon-3.svg'
 import icon4 from '../assets/Icon-4.svg'
 import {Link} from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const SimpleSteps = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 1000})
+    },[])
 
     return (
         <section className='simpleSteps' id='simpleSteps'>
@@ -17,31 +23,31 @@ const SimpleSteps = () => {
                     <img className='simpleSteps__decoration' src={decoration}/>
                     <div className='simpleSteps__content'>
                         <div className='simpleSteps__firstContent'>
-                            <img className='simpleSteps__icon1' src={icon1}/>
-                            <div className='simpleSteps__header'>Wybierz rzeczy</div>
+                            <img data-aos="fade-up" className='simpleSteps__icon1' src={icon1} alt='shirt'/>
+                            <div  data-aos="flip-left" className='simpleSteps__header'>Wybierz rzeczy</div>
                             <span> </span>
-                            <p className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
+                            <p data-aos="flip-left" className='simpleSteps__text'>ubrania, zabawki, sprzęt i inne</p>
                         </div>
                         <div className='simpleSteps__secContent'>
-                            <img className='simpleSteps__icon2' src={icon2}/>
-                            <div className='simpleSteps__header'>Spakuj je</div>
+                            <img data-aos="fade-down" className='simpleSteps__icon2' src={icon2} alt="bag"/>
+                            <div data-aos="flip-up" className='simpleSteps__header'>Spakuj je</div>
                             <span> </span>
-                            <p className='simpleSteps__text'>skorzystaj z worków na śmieci</p>
+                            <p data-aos="flip-up" className='simpleSteps__text'>skorzystaj z worków na śmieci</p>
                         </div>
                         <div className='simpleSteps__thirdContent'>
-                            <img className='simpleSteps__icon3' src={icon3}/>
-                            <div className='simpleSteps__header'>Zdecyduj komu chcesz pomóc</div>
+                            <img data-aos="zoom-in-left" className='simpleSteps__icon3' src={icon3} alt='loop'/>
+                            <div data-aos="flip-up" className='simpleSteps__header'>Zdecyduj komu chcesz pomóc</div>
                             <span> </span>
-                            <p className='simpleSteps__text'>wybierz zaufane miejsce</p>
+                            <p data-aos="flip-up" className='simpleSteps__text'>wybierz zaufane miejsce</p>
                         </div>
                         <div className='simpleSteps__fourContent'>
-                            <img className='simpleSteps__icon4' src={icon4}/>
-                            <div className='simpleSteps__header'>Zamów kuriera</div>
+                            <img data-aos="zoom-out-down"  className='simpleSteps__icon4' src={icon4} alt='around'/>
+                            <div data-aos="flip-left" className='simpleSteps__header'>Zamów kuriera</div>
                             <span> </span>
-                            <p className='simpleSteps__text'>kurier przyjedzie w dogodnym terminie</p>
+                            <p data-aos="flip-left" className='simpleSteps__text'>kurier przyjedzie w dogodnym terminie</p>
                         </div>
                     </div>
-                    <button className="big-btn"><Link to="Login">ODDAJ<br/>RZECZY</Link></button>
+                    <button data-aos="flip-left" className="big-btn"><Link to="Login">ODDAJ<br/>RZECZY</Link></button>
                 </div>
             </div>
         </section>
